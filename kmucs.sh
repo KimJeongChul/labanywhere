92f1ff8354a82f8ee59d670afe8cd55f71d8bc02
minwook <minwook0106@gmail.com>
2017-09-17 21:10:27 +0900

    apt-get to apt
    apt is designed for end-users

--- kmucs.sh
+++ kmucs.sh
@@ -3,45 +3,45 @@
 # Change apt repository
 sudo sed -i 's/kr.archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# update/upgrade system
+sudo apt -y update && sudo apt -y upgrade
 
 # Install VIM
-sudo apt-get -y install vim
+sudo apt -y install vim
 
 # Install gcc
-sudo apt-get -y install build-essential
+sudo apt -y install build-essential
 
 # Install Java oracle-8
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get -y update
+sudo apt -y update
 sudo debconf-set-selections <<< 'oracle-java8-installer shared/accepted-oracle-licnese-v1-1 select true'
-sudo apt-get -y install oracle-java8-installer
+sudo apt -y install oracle-java8-installer
 
 # Install Eclipse
-sudo apt-get -y install eclipse
+sudo apt -y install eclipse
 
 # Install R
-sudo apt-get -y install r-base
+sudo apt -y install r-base
 
 # Install Git
-sudo apt-get -y install git
+sudo apt -y install git
 
 # Install RBTools
-sudo apt-get -y install python-rbtools
+sudo apt -y install python-rbtools
 
 # Install MySQL
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password kmucs'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwordi_again kmucs'
-sudo apt-get -y install mysql-server
+sudo apt -y install mysql-server
 
 # Install MySQL Workbench
-sudo apt-get -y install mysql-workbench
+sudo apt -y install mysql-workbench
 
 # Install MySQL JDBC
-sudo apt-get -y install libmysql-java 
+sudo apt -y install libmysql-java 
 # Install django
-sudo apt-get -y install python3-pip
+sudo apt -y install python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install django
 
@@ -79,10 +79,10 @@ sudo rm spark-2.2.0-bin-hadoop2.7.tgz
 sudo ln -s /usr/local/spark-2.2.0-bin-hadoop2.7 /usr/local/spark
 
 # Enable apt over https
-sudo apt-get -y install apt-transport-https
+sudo apt -y install apt-transport-https
 
 # Install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get -y install sbt
+sudo apt update
+sudo apt -y install sbt
