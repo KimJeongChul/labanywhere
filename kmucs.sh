f704cb61c0f56bc8cc39d2129705840cefa251e7
minwook <minwook0106@gmail.com>
2017-10-24 15:51:09 +0900

    add comment, improve notify
    * Add mysql password notification
    * Add bash comment
    * Change variable name
    * Cleanup code
    * And more...

--- kmucs.sh
+++ kmucs.sh
@@ -1,56 +1,102 @@
-#!/bin/bash
-# start
-notify-send  "Labanywhere" "Installation will begin. Please exit the other running programs." -u critical
-# logging msg
-notify-send  "Labanywhere" "All installation progress is recorded in the log file." -u critical
+#!/bin/bash 
+#=================================================================================== 
+# # FILE: kmucs.sh 
+# # USAGE: chmod +x kmucs.sh;./kmucs.sh
+# # DESCRIPTION: 표준 환경 목록에 따른 Hello World 파일 
+#===================================================================================
 
-# logging part
-DATELOG=`date "+%y%d%m"`
-log="Labanywhere_script_$DATELOG.log"
+#----------------------------------------------------------------------
+# path information
+#----------------------------------------------------------------------
+local_path="/usr/local"
+
+#----------------------------------------------------------------------
+# mirror information
+#----------------------------------------------------------------------
+main_mirror="ftp.daumkakao.com"
+apache_mirror="ftp.daumkakao.com"
+pip_mirror="ftp.daumkakao.com"
+
+#----------------------------------------------------------------------
+# sourcelist information
+#----------------------------------------------------------------------
+java_ppa="http://ppa.launchpad.net/webupd8team/java/ubuntu"
+sbt_ppa="https://dl.bintray.com/sbt/debian"
+
+#----------------------------------------------------------------------
+# hadoop information
+#----------------------------------------------------------------------
+hadoop_name="hadoop"
+hadoop_version="2.8.1"
+hadoop_url="http://$apache_mirror/apache/hadoop/common/hadoop-2.8.1/hadoop-2.8.1.tar.gz"
+hadoop_zip="hadoop-2.8.1.tar.gz"
+
+#----------------------------------------------------------------------
+# spark information
+#----------------------------------------------------------------------
+spark_name="spark"
+spark_version="2.2.0-bin-hadoop2.7"
+spark_url="http://$apache_mirror/apache/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz"
+spark_zip="spark-2.2.0-bin-hadoop2.7.tgz"
+
+#----------------------------------------------------------------------
+# log information
+#----------------------------------------------------------------------
+datelog=`date "+%y%m%d"`
+log="Labanywhere_script_$datelog.log"
 kernel_version=`uname -r`
 
-function log_init(){
+#=== FUNCTION ================================================================== 
+# NAME: init_log
+# DESCRIPTION: Preparing to initialize log records.
+# Globals variable: log
+# Globals variable: kernel_version
+# Globals variable: datalog
+#===============================================================================
+function init_log(){
     echo "===============================================================">$log
-    echo "Labanywhere script $DATELOG report ">>$log
+    echo "Labanywhere script $datelog report ">>$log
     echo `date`>>$log
     echo " ">>$log
     lsb_release -a>>$log
     echo "kernerl:	$kernel_version">>$log
     echo "===============================================================">>$log
-}
+}	# ---------- end of function init_log ---------
+
+#=== FUNCTION ================================================================== 
+# NAME: install_logging
+# DESCRIPTION: Record installed programs.
+# PARAMETER 1: program name
+# Globals variable: log
+#===============================================================================
 function install_logging(){
     local value=$1
     echo "program installed : \"$1\"">>$log
     echo "---------------------------------------------------------------">>$log
-}
+}	# ---------- end of function install_logging ---------
+
+#=== FUNCTION ================================================================== 
+# NAME: delete_logging
+# DESCRIPTION: Record deleted files.
+# PARAMETER 1: Compressed file in function cleanup()
+# Globals variable: log
+#===============================================================================
 function delete_logging(){
     local value=$1
     echo "temporary file removed : \"$1\"">>$log
     echo "---------------------------------------------------------------">>$log
-}
-# path
-local_path="/usr/local"
-main_download_mirror="ftp.daumkakao.com"
-apache_download_mirror="ftp.daumkakao.com"
-pip_download_mirror="ftp.daumkakao.com"
+}	# ---------- end of function delete_logging ---------
 
-# sourcelist info
-check_java_ppa="http://ppa.launchpad.net/webupd8team/java/ubuntu"
-check_sbt_ppa="https://dl.bintray.com/sbt/debian"
-
-# hadoop info
-hadoop_name="hadoop"
-hadoop_version="2.8.1"
-hadoop_url="http://$apache_download_mirror/apache/hadoop/common/hadoop-2.8.1/hadoop-2.8.1.tar.gz"
-hadoop_zip="hadoop-2.8.1.tar.gz"
-
-# spark info
-spark_name="spark"
-spark_version="2.2.0-bin-hadoop2.7"
-spark_url="http://$apache_download_mirror/apache/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz"
-spark_zip="spark-2.2.0-bin-hadoop2.7.tgz"
-
-# If the program exists, wget re-downloading is stopped
+#=== FUNCTION ================================================================== 
+# NAME: wget_install
+# DESCRIPTION: Installing programs with wget.
+#              If the program exists, wget re-downloading is stopped.
+# PARAMETER 1: program name
+# PARAMETER 2: progeam version
+# PARAMETER 3: program URL
+# PARAMETER 4: Compressed file name
+# Globals variable: local_path
+#===============================================================================
 function wget_install(){
     local value=$1
     local value=$2
@@ -65,42 +111,83 @@ function wget_install(){
     else 
         install_logging $1
     fi
-}
+}	# ---------- end of function wget_install ---------
 
-function apt_install(){ 
-    local value=$1   
+#=== FUNCTION ================================================================== 
+# NAME: apt_install
+# DESCRIPTION: Installing programs with apt.
+# PARAMETER 1: program name(ex:vim)
+# Globals variable: log
+#===============================================================================
+function apt_install(){
+    local value=$1
     sudo apt-get -y install $1 | tee -a $log 2>&1
     install_logging $1 2>&1
-}
+}	# ---------- end of function apt_install ---------
 
+#=== FUNCTION ================================================================== 
+# NAME: pip_install
+# DESCRIPTION: Installing programs with pip.
+# PARAMETER 1: program name(ex:numpy)
+# Globals variable 1: pip_mirror
+# Globals variable 2: log
+#===============================================================================
 function pip_install(){ 
     local value=$1   
-    sudo pip3 install $1 -i http://$pip_download_mirror/pypi/simple --trusted-host $pip_download_mirror | tee -a $log 2>&1
+    sudo pip3 install $1 -i http://$pip_mirror/pypi/simple --trusted-host $pip_mirror | tee -a $log 2>&1
     install_logging $1 2>&1
-}
+}	# ---------- end of function pip_install ---------
 
-# remove temporary files
-function tmp_remove(){
+#=== FUNCTION ================================================================== 
+# NAME: cleanup
+# DESCRIPTION: deleted temporary files.
+# PARAMETER 1: Compressed file
+#===============================================================================
+function cleanup(){
     local value=$1 
     sudo rm $1 > /dev/null 2>&1
     delete_logging $1
-}
+}	# ---------- end of function cleanup ---------
 
+
+#----------------------------------------------------------------------
+# start notify
+#----------------------------------------------------------------------
+notify-send  "Labanywhere" "Installation will begin. Please exit the other running programs." -u critical
+#----------------------------------------------------------------------
+# logging notify
+#----------------------------------------------------------------------
+notify-send  "Labanywhere" "All installation progress is recorded in the log file." -u critical
+
+#----------------------------------------------------------------------
 # Change apt repository
-sudo sed -i "s/kr.archive.ubuntu.com/$main_download_mirror/g" /etc/apt/sources.list
-# logging system start
-log_init
+#----------------------------------------------------------------------
+sudo sed -i "s/kr.archive.ubuntu.com/$main_mirror/g" /etc/apt/sources.list
+
+#----------------------------------------------------------------------
+# start logging system
+#----------------------------------------------------------------------
+init_log
+
+#----------------------------------------------------------------------
 # update/upgrade system
+#----------------------------------------------------------------------
 sudo apt -y update && sudo apt -y upgrade
 
+#----------------------------------------------------------------------
 # Install VIM
+#----------------------------------------------------------------------
 apt_install vim
 
+#----------------------------------------------------------------------
 # Install gcc
+#----------------------------------------------------------------------
 apt_install build-essential
 
+#----------------------------------------------------------------------
 # Install Java oracle-8
-if ! grep -q "^deb .*$check_java_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+#----------------------------------------------------------------------
+if ! grep -q "^deb .*$java_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     sudo add-apt-repository -y ppa:webupd8team/java
 fi
 
@@ -115,64 +202,104 @@ if type javac >/dev/null 2>/dev/null; then
     install_logging java | tee -a $log 2>&1
 fi
 
+#----------------------------------------------------------------------
 # Install Eclipse
+#----------------------------------------------------------------------
 apt_install eclipse
 
+#----------------------------------------------------------------------
 # Install R
+#----------------------------------------------------------------------
 apt_install r-base
 
+#----------------------------------------------------------------------
 # Install Git
+#----------------------------------------------------------------------
 apt_install git
 
+#----------------------------------------------------------------------
 # Install RBTools
+#----------------------------------------------------------------------
 apt_install python-rbtools
 
+#----------------------------------------------------------------------
 # Install MySQL
+#----------------------------------------------------------------------
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password kmucs'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password kmucs'
 apt_install mysql-server
+#----------------------------------------------------------------------
+# mysql password notify
+#----------------------------------------------------------------------
+notify-send  "Labanywhere" "The installed mysql password is \"kmucs\"."
 
+#----------------------------------------------------------------------
 # Install MySQL Workbench
+#----------------------------------------------------------------------
 apt_install mysql-workbench
 
+#----------------------------------------------------------------------
 # Install MySQL JDBC
+#----------------------------------------------------------------------
 apt_install libmysql-java
 
+#----------------------------------------------------------------------
 # Install django
+#----------------------------------------------------------------------
 apt_install python3-pip
 sudo pip3 install --upgrade pip
 pip_install django
 
+#----------------------------------------------------------------------
 # Install numpy
+#----------------------------------------------------------------------
 pip_install numpy
 
+#----------------------------------------------------------------------
 # Install scipy
+#----------------------------------------------------------------------
 pip_install scipy
 
+#----------------------------------------------------------------------
 # Install matplotlib
+#----------------------------------------------------------------------
 pip_install matplotlib
 
+#----------------------------------------------------------------------
 # Install pyyaml
+#----------------------------------------------------------------------
 pip_install pyyaml
 
+#----------------------------------------------------------------------
 # Install nltk
+#----------------------------------------------------------------------
 pip_install nltk
 
+#----------------------------------------------------------------------
 # Install scikit-learn
+#----------------------------------------------------------------------
 pip_install scikit-learn
 
+#----------------------------------------------------------------------
 # Install hadoop
-wget_install $hadoop_name $hadoop_version $hadoop_url $hadoop_zip | tee -a $log
 # Have to set eclipse to compile hadoop in eclipse
+#----------------------------------------------------------------------
+wget_install $hadoop_name $hadoop_version $hadoop_url $hadoop_zip | tee -a $log
 
+#----------------------------------------------------------------------
 # Install spark
+#----------------------------------------------------------------------
 wget_install $spark_name $spark_version $spark_url $spark_zip | tee -a $log
 
+#----------------------------------------------------------------------
 # Enable apt over https
+#----------------------------------------------------------------------
 apt_install apt-transport-https
 
+#----------------------------------------------------------------------
 # Install sbt
-if ! grep -q "^deb .*$check_sbt_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+#----------------------------------------------------------------------
+if ! grep -q "^deb .*$sbt_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 fi
 if ! type sbt; then
@@ -185,13 +312,17 @@ if type sbt >/dev/null 2>/dev/null; then
     install_logging sbt | tee -a $log
 fi
 
-# remove temporary files
+#----------------------------------------------------------------------
+# Cleanup temporary files
+#----------------------------------------------------------------------
 if [ -f "$hadoop_zip" ]; then
-tmp_remove $hadoop_zip
+cleanup $hadoop_zip
 fi
 if [ -f "$spark_zip" ]; then
-tmp_remove $spark_zip
+cleanup $spark_zip
 fi
 
-# end
+#----------------------------------------------------------------------
+# end notify
+#----------------------------------------------------------------------
 notify-send  "Labanywhere" "This script completed successfully. Please reboot your computer." -u critical
